7e797619e0fec739d7dd37ddeeaf50e1d40551ad
Francis Lewis <francis.lewis@me.com>
2019-04-16 15:26:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,14 +7,29 @@ class Dog
     @name
   end
 
-  def breed=(new_breed)
-    @breed = new_breed
+  def job=(new_job)
+    @job = new_job
   end
 
-  def breed
-    @breed
+  def job
+    @job
   end
 end
 
 class Person
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(new_job)
+    @job = new_job
+  end
+
+  def job
+    @job
+  end
 end
